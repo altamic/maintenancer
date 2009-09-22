864ab8d367fda6ec9b0b4c33d190eca4870dd504
Michelangelo Altamore <michelangelo@altamore.org>
2009-09-22 18:43:08 +0200

    updated filter panel

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$(function(){
-	// Datepicker
-	$('.date').datepicker();
-	
-	$('toggle_control').toggle(
-	  function() {$('.toggle').show()},
-	  function() {$('.toggle').hide()}
-	  );
-});
+
+(function($) { 
+
+  var show_filter_panel = function () {
+    $('fieldset.toggle').slideDown();
+  }
+  
+  var hide_filter_panel = function () {
+    $('fieldset.toggle').slideUp();
+  }
+  
+  $(function(){
+  	// Datepicker
+  	$('.date').datepicker();
+    $('fieldset.toggle').hide();
+    // Filter panel toggle commandd
+  	$('#filter_panel_link').toggle(show_filter_panel, hide_filter_panel);
+  });
+
+})(jQuery); 
 
 function changeLocation(url){
   document.location.href=url;
