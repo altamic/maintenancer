8ad2128723bca8ab54a4cc1ae6e240f09beb03c2
Michelangelo Altamore <michelangelo@altamore.org>
2009-09-24 14:39:35 +0200

    updated rails to 2.3.4

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,11 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "authlogic"
+  # Add additional load paths for your own custom dirs
+  # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  config.gem "authlogic"  , :version => ">= 2.1.2"
+  config.gem "searchlogic", :version => ">= 2.3.3"
+
   config.time_zone = 'Rome'
   config.i18n.default_locale = :it
 end
